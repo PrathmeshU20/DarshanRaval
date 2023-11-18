0968b05d62d2b4b43ee7f8d2f164ff6087424214
PrathmeshU20 <prathmeshurkude20@gmail.com>
2023-11-18 23:01:57 +0530

    Added routes and empty views

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view("/", 'home')->name('home');
+Route::view("/about", 'about')->name('about');
+Route::view("/history", 'history')->name('history');
+Route::view("/books", 'books')->name('books');
+Route::view("/contact", 'contact')->name('contact');
 
-Route::get('/', function () {
-    return view('welcome');
-});
